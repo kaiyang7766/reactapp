7dd3e8a1cb569de05ddfa5ec4f9833621c9b8b35
Kai Yang <yangkyx@gmail.com>
2020-07-21 09:37:08 +0800

    Updates

---
+++ precache-manifest.4eb2e4a7b52318187c0bf563fad08ecf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ea556eddaf63de4dce4c1a011cae3f",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "8b8c81bcf6a2c37907ce",
+    "url": "/reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/reactapp/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b8c81bcf6a2c37907ce",
+    "url": "/reactapp/static/js/main.d38ade61.chunk.js"
+  },
+  {
+    "revision": "7325850d54f596a589d5",
+    "url": "/reactapp/static/js/runtime-main.f9e8ea19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
